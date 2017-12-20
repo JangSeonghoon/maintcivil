42640c19e9fe9cf1215b5ab6242262f9a42f02c2
JangSeonghoon <jangsh8181@naver.com>
2017-12-20 10:27:01 +0900

    1

--- R/inputInspectTable.R
+++ R/inputInspectTable.R
@@ -8,9 +8,9 @@ quater=4
 
 #'
 #' create Inspect Table
-#' 
+#'
 #' @param year, quater,workspace_no
-#' @return 
+#' @return
 devtools::use_package("magrittr")
 devtools::use_package("dplyr")
 devtools::use_package("csvread")
@@ -25,11 +25,12 @@ devtools::use_package("RJDBC")
 #'@importFrom DBI dbConnect
 #'@importFrom DBI dbWriteTable
 #'@importFrom DBI dbDisconnect
+#'@importFrom compiler cmpfun
 #'@export
 createInspect=function(year,quater,workspace_no){
 A=cmpfun(
 function(){
-  
+
 quater=ifelse(nchar(quater)==1,paste0("0",quater),quater)
 coltypes=map.coltypes(
   paste0("/home/jsh/eclipse-workspace/bigTeam/src/main/webapp/RData/",workspace_no,"_",year,quater,".csv"),header=T)
---
+++ R/signalInfo.R
@@ -0,0 +1,36 @@
+#'
+#' signal information
+#'
+#' @param workspace_no, startT,lastT,direction, order, kind
+#' @return km of the signal
+devtools::use_package("stringr")
+
+#' @importFrom stringr str_c
+#' @importFrom stringr str_detect
+#' @importFrom compiler cmpfun
+#' @export
+signal=function(workspace_no,startT,lastT,direction,order,kind){
+A=cmpfun(
+function(){
+
+workspace_no=floor(workspace_no/100)*100
+
+load("/home/jsh/eclipse-workspace/bigTeam/src/main/webapp/RData/DB(utf8).RData")
+
+compare=eval(parse(text=paste0("signal_",workspace_no)))
+
+compareSet=str_c(compare[,1],collape=",",compare[,2],collape=",",compare[,3],collape=",",compare[,4],collape="번,",compare[,6],collape="")
+
+no=which(
+str_detect(compareSet,startT)*
+  str_detect(compareSet,lastT)*
+  str_detect(compareSet,direction)*
+  str_detect(compareSet,paste0(order,"번"))*
+  str_detect(compareSet,kind)==1
+)[1]
+
+compare[no,5]
+}
+)
+A()
+}
